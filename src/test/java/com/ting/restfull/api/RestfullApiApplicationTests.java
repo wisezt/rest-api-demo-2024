ee7bbfec9b4e7d33e965231eff2cda45d98fa4d9
DESKTOP-E0AM5JD\ting <wisezt@hotmail.com>
2024-01-15 11:26:16 +0800

    Initialing...

---
+++ src/test/java/com/ting/restfull/api/RestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ting.restfull.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
